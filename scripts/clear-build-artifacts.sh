969b909b8d2d01de0266bab30e0ff6863a6fdd12
Patrik Pyrzanowski <patrik.pyrzanowski@check24.de>
2025-11-03 21:20:54 +0100

    Add build artifact clean up script

---
+++ scripts/clear-build-artifacts.sh
@@ -0,0 +1,4 @@
+# Remove all dist/ folders in apps/ and packages/
+find apps packages -name "dist" -type d -exec rm -rf {} +
+# Remove all tsbuildinfo files in apps/ and packages/
+find apps packages -path "*/node_modules/.tmp/tsconfig.tsbuildinfo" -type f
